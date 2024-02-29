19404d1f6c70d37fd6d074e02573d7dfae0bb14c
Júlia Luz <108757926+ju-lialuz@users.noreply.github.com>
2024-02-29 16:11:02 -0300

    Criar funcoes de achar vizinhos e atualizar matriz

--- GameOfLife/GameOfLife/ContentView.swift
+++ GameOfLife/GameOfLife/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var matriz: [[Int]] = []
+//    @State var para: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            
+            ForEach( matriz.indices, id:\.self ) { i in
+                HStack{
+                    ForEach( matriz.indices, id:\.self ){ j in
+                        Rectangle()
+                            .frame(width: 30, height: 30)
+                            .foregroundStyle(matriz[i][j] == 1 ? .pink : .gray)
+                    }
+                }
+            }
+            
+            Button{
+                matriz = MatrizGameOfLife()
+                print(matriz)
+            } label: {
+                Text("Matriz")
+            }
+            
+            Button{
+                matriz = atualizaMatriz( matriz: matriz)
+            } label: {
+                Text("Play")
+            }
         }
-        .padding()
+        .onAppear {
+            
+//            if !para{
+//                matriz = MatrizGameOfLife()
+//                para.toggle()
+//            }
+            
+        }
+        
+        
     }
+    
+    
 }
-
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
+    
+    
--- GameOfLife/GameOfLife/MatrizGameOfLife.swift
+++ GameOfLife/GameOfLife/MatrizGameOfLife.swift
@@ -7,17 +7,85 @@
 
 import Foundation
 
-func MatrizGameOfLife(){
+func MatrizGameOfLife() -> [[Int]]{
     var matriz: [[Int]] = []
-    
     var rows: [Int] = []
     
-    for i in 0...5 {
-        for j in 0...5 {
+    for _ in 0...4 {
+        for _ in 0...4 {
             rows.append(Int.random(in: 0...1))
         }
         matriz.append(rows)
         rows.removeAll()
     }
     
+    return matriz
+}
+
+func findNeighborhoods(i: Int, j: Int, matriz: [[Int]]) -> Int {
+    var qtd: Int = 0
+    
+    if matriz.count > 0 && matriz[0].count > 0 {
+        for ml in max(0, i - 1)...min(i + 1, matriz.count - 1){
+            for mc in max(0, j - 1)...min(j + 1, matriz.count - 1){
+                if !(i == ml && j == mc) {
+                    if matriz[ml][mc] == 1 {
+                        qtd += 1
+                    }
+                }
+                
+            }
+        }
+                
+
+    }
+    
+    return qtd
+}
+
+func rules(i: Int, j: Int, matriz: [[Int]]) -> Int {
+    
+    let qtd = findNeighborhoods(i: i, j: j, matriz: matriz)
+    var life = matriz[i][j]
+    
+    if life == 1 {
+        if qtd < 2 || qtd > 3{
+            life = 0
+        } else if qtd == 2 || qtd == 3 {
+            life = 1
+            
+        }
+        
+    }else {
+        if qtd == 3 {
+            life = 1
+            
+        }
+    }
+    
+    return life
+
+    
+}
+
+func atualizaMatriz( matriz: [[Int]]) -> [[Int]] {
+    var matrizAtualizada: [[Int]] = []
+    
+    var linhaAtualizada: [Int] = []
+    
+    
+    for i in 0...matriz.count - 1 {
+        for j in 0...matriz[i].count - 1{
+            let estado = rules(i: i, j: j, matriz: matriz)
+            linhaAtualizada.append(estado)
+            
+            
+        }
+        matrizAtualizada.append(linhaAtualizada)
+        
+        linhaAtualizada.removeAll()
+   
+        
+    }
+    return matrizAtualizada
 }
